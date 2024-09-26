694c8a8a2e0f6af92fd6d4a119efc81417d4e1f0
throwaway008 <trow0827@gmail.com>
2024-09-26 20:18:22 +0800

    Update restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -1,3 +1,6 @@
+# Eto lang yung basic functions ng mga code, sa weekend natin planuhin kung ano yung mga ilalagay natin. I think
+# Menu lang yung pinaka important based dun sa Rubric kaya kahit siguro dina masyadong complex yung code
+
 def show_main_menu():
     """
     1. View Menu
@@ -7,11 +10,11 @@ def show_main_menu():
 
 def view_menu():
     """
-    Categories
-    Categories
-    Categories
+    Show each menu directly || Seperate by Category
     """
 
+# Only if Category will be implemented
+# Dependent on complexity
 def show_category():
     """
     Menu in specific Category
@@ -20,16 +23,17 @@ def show_category():
 def place_order():
     """
     Adding and Deleting Items
+    ie. add_item(), delete_item()
     """
 
 def add_item():
     """
-    Add
+    Add item
     """
 
 def delete_item():
     """
-    Delete
+    Delete item
     """
 
 def confirm_order():
@@ -44,6 +48,8 @@ def process_payment():
 def main():
     """
     System Logic Starts Here
+
+    Di pa alam anong gagawin (Weekend)
     """
 
 if __name__ == "__main__":
