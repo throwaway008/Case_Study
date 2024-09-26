7df79e4cc850572cf7226453efcf00e8d742c582
throwaway008 <trow0827@gmail.com>
2024-09-26 16:02:30 +0800

    Create restaurant.py

---
+++ restaurant.py
@@ -0,0 +1,50 @@
+def show_main_menu():
+    """
+    1. View Menu
+    2. Place Order
+    3. Exit Restaurant
+    """
+
+def view_menu():
+    """
+    Categories
+    Categories
+    Categories
+    """
+
+def show_category():
+    """
+    Menu in specific Category
+    """
+
+def place_order():
+    """
+    Adding and Deleting Items
+    """
+
+def add_item():
+    """
+    Add
+    """
+
+def delete_item():
+    """
+    Delete
+    """
+
+def confirm_order():
+    """
+    ???
+    """
+def process_payment():
+    """
+    ???
+    """
+
+def main():
+    """
+    System Logic Starts Here
+    """
+
+if __name__ == "__main__":
+    main()
